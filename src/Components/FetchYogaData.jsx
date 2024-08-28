deb82e6a2fb1244f7a14ffb58b7d0ff542c3bab0
ElijahBelov <mewoak@gmail.com>
2024-08-28 14:29:57 -0400

    yoga

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -13,14 +13,18 @@ const FetchYogaData = () => {
 
     let output = [];
 
-    output.push(<h3>{myArrayObj[0][1]}</h3>)
+    output.push(<h2>{myArrayObj[0][1]}</h2>)
     myArrayObj.splice(0, 1);
 
     for(const myPair in myArrayObj){
         if (myArrayObj[myPair][1].startsWith('https://')){
             output.push(<img className="fetch_Image" src={myArrayObj[myPair][1] } alt="Image not found"/>);
         } else {
-            output.push(<p><strong>{myArrayObj[myPair][0].replace(/_|-/g, " ")}</strong> : {myArrayObj[myPair][1]}</p>);        
+            output.push(<><h3>{myArrayObj[myPair][0].replace(/_|-/g, " ")}</h3><p>{myArrayObj[myPair][1]}</p></>);        
+        //            const words = myArrayObj[myPair][1].split(", ");
+        //    for(const benefit in words){
+        //        output.push(<li>{words[benefit]}</li>);
+        //    } 
         }
     }
 
